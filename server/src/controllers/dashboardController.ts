14018d03bec2e06aecea2a73bc2d75e71c1fd5c3
tusharranker1 <98636021+tusharranker1@users.noreply.github.com>
2025-07-10 19:04:47 +0530

    third commit

--- server/src/controllers/dashboardController.ts
+++ server/src/controllers/dashboardController.ts
@@ -41,7 +41,7 @@ export const getDashboardMetrics = async (
       }
     );
     const expenseByCategorySummary = expenseByCategorySummaryRaw.map(
-      (item) => ({
+      (item : any) => ({
         ...item,
         amount: item.amount.toString(),
       })
