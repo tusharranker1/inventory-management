14a5cb9449b6c2edc15a97f5ffd905bb21999472
tusharranker1 <98636021+tusharranker1@users.noreply.github.com>
2025-07-10 19:02:26 +0530

    second commit

--- server/src/controllers/expenseController.ts
+++ server/src/controllers/expenseController.ts
@@ -7,18 +7,22 @@ export const getExpenses = async (
   req: Request,
   res: Response
 ): Promise<void> => {
-    try {
-        const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany({
-            orderBy:{
-                date:"desc",
-            },
-        })
-        const expenseByCategorySummary = expenseByCategorySummaryRaw.map((item)=>({
-            ...item,
-            amount:item.amount.toString()
-        }))
-        res.json(expenseByCategorySummary);
-    } catch (error) {
-        res.status(500).json({ message: "Error retrieving expenses by category" });
-    }
-}
+  try {
+    const expenseByCategorySummaryRaw = await prisma.expenseByCategory.findMany(
+      {
+        orderBy: {
+          date: "desc",
+        },
+      }
+    );
+    const expenseByCategorySummary = expenseByCategorySummaryRaw.map(
+      (item: any) => ({
+        ...item,
+        amount: item.amount.toString(),
+      })
+    );
+    res.json(expenseByCategorySummary);
+  } catch (error) {
+    res.status(500).json({ message: "Error retrieving expenses by category" });
+  }
+};
